88b3039ea466d7cd13c3594121b0303e4ff4b77e
Robert Collins <robertco@ie.ibm.com>
2015-09-27 22:12:50 +0800

    Ran pod update and install.

---
+++ Pods/Target Support Files/Pods-ExampleUIAndMockHTTPTestingUITests/Pods-ExampleUIAndMockHTTPTestingUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ExampleUIAndMockHTTPTestingUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ExampleUIAndMockHTTPTestingUITestsVersionString[];
+
