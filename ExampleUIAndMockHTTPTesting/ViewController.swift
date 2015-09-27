241ad46253fb9f2d003734d8103587e1ac85843b
Robert Collins <robertco@ie.ibm.com>
2015-09-27 21:44:38 +0800

    Added a simple UI and ViewController implementation.

--- ExampleUIAndMockHTTPTesting/ViewController.swift
+++ ExampleUIAndMockHTTPTesting/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func makeRequest() {
+        let url = NSURL(string: "https://bing.com")!
+        let request = NSURLRequest(URL: url)
+        let session = NSURLSession.sharedSession()
+        let dataTask = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+            let httpResponse = response as? NSHTTPURLResponse
+            let status = httpResponse?.statusCode ?? 0
+            dispatch_async(dispatch_get_main_queue()) {
+                self.resultLabel.text = "\(status)"
+            }
+        }
+        dataTask.resume()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
