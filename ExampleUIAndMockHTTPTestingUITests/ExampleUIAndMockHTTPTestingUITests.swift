28aa066baf1d8c074a7fc6cdffc164e8cf09ba31
Robert Collins <robertco@ie.ibm.com>
2015-09-27 22:21:25 +0800

    Added an example test with a mock.

--- ExampleUIAndMockHTTPTestingUITests/ExampleUIAndMockHTTPTestingUITests.swift
+++ ExampleUIAndMockHTTPTestingUITests/ExampleUIAndMockHTTPTestingUITests.swift
@@ -7,7 +7,38 @@
 //
 
 import XCTest
+import OHHTTPStubs
 
 class ExampleUIAndMockHTTPTestingUITests: XCTestCase {
-
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        OHHTTPStubs.removeAllStubs()
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Given
+        let app = XCUIApplication()
+        OHHTTPStubs.stubRequestsPassingTest({$0.URL!.host == "bing.com"}) { _ in
+            return OHHTTPStubsResponse(data: NSData(), statusCode: 404, headers: nil)
+        }
+        
+        let requestButton = app.buttons["Make HTTP Request"]
+        
+        // When
+        requestButton.tap()
+        
+        // Then
+        let expected = "404"
+        let testPredicate = NSPredicate(format: "label == '\(expected)'")
+        let object = app.staticTexts.elementMatchingType(.Any, identifier: "resultLabel")
+        
+        expectationForPredicate(testPredicate, evaluatedWithObject: object, handler: nil)
+        waitForExpectationsWithTimeout(5, handler: nil)
+    }
 }
